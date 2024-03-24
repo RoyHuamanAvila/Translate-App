f80893be2e6c8c0d7ae025314f7a689685d393f6
Roy Andres <rhavila789@gmail.com>
2024-03-24 16:42:11 -0500

    Agregadas interfaces de respuesta en Types/index.ts

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -48,3 +48,36 @@ export interface GetRequestAPI {
   de?: string;
   user?: string;
 }
+
+export interface Response {
+  responseData: ResponseData;
+  quotaFinished: boolean;
+  mtLangSupported: any;
+  responseDetails: string;
+  responseStatus: number;
+  responderId: any;
+  exception_code: any;
+  matches: Match[];
+}
+
+export interface ResponseData {
+  translatedText: string;
+  match: number;
+}
+
+export interface Match {
+  id: string;
+  segment: string;
+  translation: string;
+  source: string;
+  target: string;
+  quality: number;
+  reference: any;
+  "usage-count": number;
+  subject: string;
+  "created-by": string;
+  "last-updated-by": string;
+  "create-date": string;
+  "last-update-date": string;
+  match: number;
+}
