2a516b6d38662525f44be7631568b6b069c61843
Roy Andres <rhavila789@gmail.com>
2024-04-03 21:24:55 -0500

    Actualizar vista de traducción de tarjeta y estilos de opción múltiple

--- src/Components/MultiOption/MultiOption.tsx
+++ src/Components/MultiOption/MultiOption.tsx
@@ -1,37 +1,56 @@
 import "./MultiOption.css"
-import { FC, FormEvent } from "react"
+import { FC, FormEvent, useEffect, useState } from "react"
 
 interface MultiOptionProps {
   name: string;
   options: string[];
   prefix?: string;
-  onClick?: (e: FormEvent<HTMLInputElement>) => void;
-  currentOption?: string;
+  onClick: (e: FormEvent<HTMLInputElement>) => void;
+  currentOption: string;
 }
 
 const MultiOption: FC<MultiOptionProps> = ({ name, options, prefix, onClick, currentOption }) => {
+  const [radioOptions, setRadioOptions] = useState<string[]>([])
+  const [selectOptions, setSelectOptions] = useState<string[]>([])
+
+  useEffect(() => {
+    setRadioOptions(options.slice(0, 3));
+    setSelectOptions(options.slice(3));
+  }, [])
+
+  const handleSelectChange = (e: FormEvent<HTMLSelectElement>) => {
+    onClick(e as any);
+  }
+
   return (
     <div className="multi-option">
-      {
-        options.map(option => {
-          const fixedID = `${prefix}_${option}`
-          return (
-            <div className="option" key={fixedID}>
-              <input
-                id={fixedID}
-                type="radio"
-                className="option-input"
-                name={name}
-                onClick={onClick}
-                value={option}
-                checked={option === currentOption}
-                readOnly
-              />
-              <label htmlFor={fixedID}>{option}</label>
-            </div>
-          )
-        })
-      }
+      <div className="multi-option-container">
+        {
+          radioOptions.map(option => {
+            const fixedID = `${prefix}_${option}`
+            return (
+              <div className="option" key={fixedID}>
+                <input
+                  id={fixedID}
+                  type="radio"
+                  className="option-input"
+                  name={name}
+                  onClick={onClick}
+                  value={option}
+                  checked={option === currentOption}
+                  readOnly
+                />
+                <label htmlFor={fixedID}>{option}</label>
+              </div>
+            )
+          })
+        }
+      </div>
+      <select className={`multi-option-select ${selectOptions.includes(currentOption) ? 'active' : ''}`} name={name} title={name} onChange={handleSelectChange}>
+        {selectOptions.map((option, index) => (
+          <option key={index} value={option}>{option}</option>
+        ))}
+      </select>
     </div>
   )
 }
